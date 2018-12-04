ef3420d18efd850bfca9e4d6cd39fe67b1080077
tigi44 <onlytg@naver.com>
2018-12-04 15:49:04 +0900

    Init SpringBootAdminExample

---
+++ SpringBootAdminServer/src/test/java/com/springBootAdminServer/SpringBootAdminServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springBootAdminServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAdminServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
